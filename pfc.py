9306a4b64128c76b858d118b312fccad417be30b
Terazix <147041328+Terazix@users.noreply.github.com>
2023-11-23 10:56:15 +0100

    Add files via upload

--- pfc.py
+++ pfc.py
@@ -1,30 +1,30 @@
 import random
 
 def restart():
-    o = input("vouler vous rejouer? ")
-    if o == "oui":
+    yes = input("vouler vous rejouer? ")
+    if yes == "oui":
         pfc()
     else:
         print("au revoir")
+
 def pfc():
     print("bienvenue dans le jeux du pierre feuille ciseaux")
-    choix = ["pierre","feuille","ciseau"]
+    choice = ["pierre","feuille","ciseau"]
 
     while True:
-        joueur= input("choissiser pierre feuille ou ciseau pour jouer contre l'ordi ")
+        user = input("choissiser pierre feuille ou ciseau pour jouer contre l'ordi ")
 
-
-        if joueur not in choix:
+        if user not in choice:
             print("veuiller saisir la bonne chose")
             continue
 
-        ordinateur = random.choice(choix)
+        computer = random.choice(choice)
 
-        print(f"l'ordinateur a choisit {ordinateur}")
-        if joueur == ordinateur:
+        print(f"l'ordi a choisit {computer}")
+        if user == computer:
                 print("egalité")
 
-        elif    (joueur == "pierre" and ordinateur == "ciseau") or (joueur == "feuille" and ordinateur == "pierre") or  (joueur == "ciseau" and ordinateur == "feuille"):
+        elif    (user == "pierre" and computer == "ciseau") or (user == "feuille" and computer == "pierre") or  (user == "ciseau" and computer == "feuille"):
                 print("vous aver gagné")
 
         else:
--- plusoumoins.py
+++ plusoumoins.py
@@ -1,51 +1,67 @@
 import random
 
-def AskInt() -> int: 
+def AskInt(message: str) -> int: 
     while True:
         try:
-            user_input = int(input(""))
+            user_input = int(input(message))
             return user_input
         except ValueError:
             print("entre un truc valide")
-      
-def plusmoin():
+
+def AskInput(message: str, choice: list[str]) -> str:
+    while True:
+        user_in = input(message)
+
+        if user_in in choice:
+            return user_in
+
+        print("champs non valide")
+        print("veuiller reessayer")
+
+        
+            
+
+
+def start(essais,max_essais,nombre,min,max):
+    
+    while essais < max_essais:
+            essai = AskInt(f"Devinez le nombre entre {min} et {max} : ")
+            essais += 1
+            if essai < nombre:
+                print("C'est plus !")
+            elif essai > nombre:
+                print("C'est moins !")
+            elif essai == nombre:
+                print(f"Félicitations ! Vous avez trouvé le nombre mystère ({nombre}) en {essais} essais")
+                break   
+            elif essais == max_essais:
+                print(f"Dommage ! Vous avez dépassé le nombre maximal d'essais. Le nombre mystère était {nombre}")
+                break
+
+
+def game():
     print("Bienvenue dans le jeu Devine le Nombre !")
     while True:
-        borne = input("Voulez-vous définir des bornes personnalisées ? (oui/non) : ")
-        
+        borne = AskInput("Voulez-vous définir des bornes personnalisées ? (oui/non) : ",["oui","non"])
         if  borne == "oui":
-            min = input("Entrez la borne minimale : ")
-            AskInt()
-            max = input("Entrez la borne maximale : ")
-            AskInt()
+            min = AskInt("Entrez la borne minimale : ")
+            max = AskInt("Entrez la borne maximale : ")
         else:
             min, max = 1, 100
         
         nombre = random.randint(min, max)
+        print(nombre)
         essais = 0
         max_essais = 10
-        
-        while essais < max_essais:
-            essai = input(f"Devinez le nombre entre {min} et {max} : ")
-            AskInt()
-            essais += 1
-
-            if essai == nombre:
-                print(f"Félicitations ! Vous avez trouvé le nombre mystère ({nombre}) en {essais} essais")
-                
-            elif essai < nombre:
-                print("C'est plus !")
-            else:
-                print("C'est moins !")
+        start(essais,max_essais,nombre,min,max)
+        rejouer = AskInput("Voulez-vous jouer à nouveau ? (oui/non) : ",["non","oui"])
+        if rejouer == "oui":
+            print("go") 
+        else: 
             break
-        else:
-            print(f"Dommage ! Vous avez dépassé le nombre maximal d'essais. Le nombre mystère était {nombre}")
-        rejouer = input("Voulez-vous jouer à nouveau ? (oui/non) : ")
-        if rejouer != "oui":
-            print("Merci d'avoir joué ! Au revoir.")
-        break
- 
-plusmoin()
+game()
+       
+
 
            
 
