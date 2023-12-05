5492107b2ec386ff0a3d0eae01306e02b950842c
Terazix <147041328+Terazix@users.noreply.github.com>
2023-12-05 12:30:37 +0100

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -8,7 +8,7 @@ def init_grid():
 
 # Fonction pour ajouter un nombre aléatoire (2 ou 4) à un emplacement vide
 def add_new(grid):
-    empty_cells = [(i, j) for i in range(3) for j in range(3) if grid[i][j] == 0]
+    empty_cells = [(i, j) for i in range(4) for j in range(4) if grid[i][j] == 0]
     if empty_cells:
         i, j = random.choice(empty_cells)
         grid[i][j] = generate()
@@ -21,8 +21,8 @@ def print_grid(grid):
 
 # fonction pour generer un nombre aleatoire dans la grille avec proba
 def generate():
-    rand = random.randint(1,100)
-    if rand < 90:
+    rand = random.randint(1,10)
+    if rand < 9:
         return 2
     else:
         return 4
@@ -30,54 +30,49 @@ def generate():
 
 
 
+def move_right(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])-1, 0, -1):
+            if grid[i][j] == grid[i][j - 1] and grid[i][j] != 0:
+                grid[i][j] *= 2
+                grid[i][j - 1] = 0
+    for i in range(len(grid)):
+        grid[i].sort(key=lambda x: 1 if x == 0 else 0, reverse=True)
+
+
 def move_left(grid):
     for i in range (len(grid)-1):
         for j in range(len(grid)-1):
-            if grid[i][j] == grid[i][j + 1] and grid[i][j] != 0:
+             if grid[i][j] == grid[i][j - 1] and grid[i][j] != 0:
                 grid[i][j] *= 2
-                grid[i][j + 1] = 0
-    grid.sort(key=lambda x: 1 if x == 0 else 0)
-
-
-def move_right(grid):
-    for row in grid:
-        for i in range (len(grid)-1):
-            for j in range(len(grid)-1):
-                if row[j] == row[j - 1] and row[j] != 0:
-                    row[j] *= 2
-                    row[j - 1] = 0
-        row.sort(key=lambda x: 1 if x == 0 else 0)
+                grid[i][j - 1] = 0
+    for i in range(len(grid)):
+        grid[i].sort(key=lambda x: 1 if x == 0 else 0)
 
 
 def move_up(grid):
-    for row in grid:
-        for i in range(len(grid)-1):
-            for j in range(len(grid)-1):
-                if row[i] == row[i + 1] and row[i] != 0:
-                    row[i] *= 2
-                    row[i + 1] = 0
-        row.sort(key=lambda x: 1 if x == 0 else 0)
+    for i in range(len(grid)):
+        for j in range(len(grid[i])-1):
+            if grid[j][i] == grid[j][i - 1] and grid[j][i] != 0:
+                grid[j][i] *= 2
+                grid[j][i - 1] = 0
+    for j in range(len(grid)):
+        grid[j].sort(key=lambda x: 1 if x == 0 else 0, reverse=True)
+
 
 def move_down(grid):
-    for row in grid:
-        for i in range(len(grid)-1):
-            for j in range(len(grid)-1):
-                if row[i] == row[i - 1] and row[i] != 0:
-                    row[i] *= 2
-                    row[i - 1] = 0
-        row.sort(key=lambda x: 1 if x == 0 else 0)
-
+    for i in range (len(grid)-1):
+        for j in range(len(grid)-1,0,-1):
+             if grid[i][j] == grid[i][j - 1] and grid[i][j] != 0:
+                grid[i][j] *= 2
+                grid[i][j - 1] = 0
+    for i in range(len(grid)):
+        grid[i].sort(key=lambda x: 1 if x == 0 else 0)
 
 def game():
     
-    AskInput("bienvenue dans le jeux du 2048 pour commencer ecriver oui sinon ecriver non",["oui","non"])
-    if AskInput == "oui":
-        print("go")
-    else:
-        print("au revoir") 
-       
+
     grid = init_grid()
-    v = len(grid) 
     add_new(grid)
     print_grid(grid)
     generate()     
@@ -86,19 +81,34 @@ def game():
     while True:
         event = keyboard.read_event()
         if event.event_type == keyboard.KEY_DOWN and event.name == "haut":
-            move_up(grid)
+            #move_up(grid)
+            add_new(grid)
             print_grid(grid)
         if event.event_type == keyboard.KEY_DOWN and event.name == "bas":
-            move_down(grid)
+            #move_down(grid)
+            add_new(grid)
             print_grid(grid)
         if event.event_type == keyboard.KEY_DOWN and event.name == "droite":
-            move_left(grid)
+            move_right(grid)
+            add_new(grid)
             print_grid(grid)
         if event.event_type == keyboard.KEY_DOWN and event.name == "gauche":
-            move_right(grid)
+            move_left(grid)
+            add_new(grid)
             print_grid(grid)
-          
-game()
+
+def param():
+    
+        yes = AskInput("bienvenue dans le jeux du 2048 pour commencer ecriver oui sinon ecriver non ",["oui","non"])
+        if yes == "oui":
+            print("go")
+            game()
+        else:
+            print("au revoir")
+
+param()
+
+
 
 
 
